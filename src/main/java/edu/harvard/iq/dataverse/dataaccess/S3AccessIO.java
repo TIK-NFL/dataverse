e7ee4f3d14df684c516c519e915a5ff3a672fc0e
Florian Fritze <florian.fritze@ub.uni-stuttgart.de>
2025-09-15 15:10:34 +0200

    logging fix

--- src/main/java/edu/harvard/iq/dataverse/dataaccess/S3AccessIO.java
+++ src/main/java/edu/harvard/iq/dataverse/dataaccess/S3AccessIO.java
@@ -126,6 +126,8 @@ public class S3AccessIO<T extends DvObject> extends StorageIO<T> {
             s3 = getClient(driverId);
             s3.serviceClientConfiguration().overrideConfiguration().toBuilder().apiCallTimeout(Duration.ofSeconds(Long.parseLong(getConfigParam(API_CALL_TIMEOUT,"6"))))
                 .apiCallAttemptTimeout(Duration.ofMillis(Long.parseLong(getConfigParam(API_CALL_ATTEMPT_TIMEOUT,"2000"))));
+            logger.info("API Call Timeout has been set to : " + getConfigParam(API_CALL_TIMEOUT,"6"));
+            logger.info("API Call Attempt Timeout has been set to : " + getConfigParam(API_CALL_ATTEMPT_TIMEOUT,"2000"));
             tm = getTransferManager(driverId);
             s3Presigner = getPresigner(driverId);
             endpoint = getConfigParam(CUSTOM_ENDPOINT_URL, "");
